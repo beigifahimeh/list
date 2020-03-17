b4fa65b00d40d8ec2dfeeb9db1e50bbbf4855de9
beigifahimeh <beigifahimeh73@gmail.com>
2020-03-17 14:53:52 +0100

    Updates

---
+++ precache-manifest.18bd2bf81e5f0bb19a5eccbf0ae415b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c173ed9899b7bf528335831bcbedd",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "29f4364a21a7a4b06d64",
+    "url": "/list/static/css/main.6582bcba.chunk.css"
+  },
+  {
+    "revision": "bce6ca94692da5bb8bbb",
+    "url": "/list/static/js/2.6c56e7c0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/list/static/js/2.6c56e7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f4364a21a7a4b06d64",
+    "url": "/list/static/js/main.d4f4999e.chunk.js"
+  },
+  {
+    "revision": "e3f6cec48f431a7ab57f",
+    "url": "/list/static/js/runtime-main.03ee1402.js"
+  }
+]);
